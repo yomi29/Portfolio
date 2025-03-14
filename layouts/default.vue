98f05203739a369cb299ff6b164c516ecde1d521
yomi29 <140530259+yomi29@users.noreply.github.com>
2025-03-14 13:19:32 +0100

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="bg-">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1 class="text-7xl font-bold underline">Hello world!</h1>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
